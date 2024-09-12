556bd2a5bbfce2454a7ad30d5dec77d9a330794b
Kaionobre <kaionobrega00@gmail.com>
2024-09-12 11:39:59 -0300

    ADD: Realizando a raspagem dos dados

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://noticias.uol.com.br/'
 
+url = 'https://www.adrenaline.com.br/noticias/'                    
 response = requests.get(url)
+raw_html = response.text
+parsed_html = BeautifulSoup(raw_html, 'html.parser')
 
-html = response.text
-html_convertido = BeautifulSoup(html, 'html.parser')
+article = parsed_html.find('div', class_='archive-list-posts')
 
-print(html_convertido.title)
+noticias = article.find_all('h2')
+
+x=1
+print('Notícias do Dia:')
+
+for notica in noticias:
+    print(f'{x}', notica.text)
+    x += 1
--- teste.py
+++
@@ -1,9 +0,0 @@
-valor_mxrf11 = 10
-quantidade = int(input(f'QTD: ')) 
-quantidade_mxrf11 = (quantidade * valor_mxrf11)
-
-rentabilidade = float(1.1)
-
-resultado = quantidade_mxrf11 * rentabilidade
-
-print(resultado)
